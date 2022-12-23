2ec125c082679843d105cfca2f3e0007b1fad7e2
SonnyX <randy@vonderweide.nl>
2022-12-23 21:11:08 +0100

    Improve performance significantly

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,74 @@
 extern crate renegadex_patcher;
 extern crate ini;
+extern crate tracing_subscriber;
 
-use std::error::Error;
-use flexi_logger::{Age, Cleanup, Criterion, Duplicate, FileSpec, Logger, Naming};
-use renegadex_patcher::{Downloader, Update};
+use std::{error::Error, sync::atomic::Ordering};
+use tracing::{info, error};
 use ini::Ini;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, Layer};
 
-fn main() -> Result<(), std::boxed::Box<dyn Error>> {
-  Logger::try_with_env_or_str("info")?
-    .format(flexi_logger::opt_format)
-    .log_to_file(FileSpec::default().directory("logs"))
-    .duplicate_to_stderr(Duplicate::Warn)
-    .rotate(Criterion::Age(Age::Day), Naming::Numbers, Cleanup::KeepLogFiles(5))
-    .print_message()
-    .start()
-    .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
+mod as_string;
+mod version_information;
 
-  let conf = match Ini::load_from_file("RenegadeX.ini") {
-    Ok(conf) => conf,
-    Err(_e) => {
-      let mut conf = Ini::new();
-      conf.with_section(Some("RenX_Patcher"))
-        .set("GameLocation", "../")
-        .set("VersionUrl", "https://static.ren-x.com/launcher_data/version/release.json");
-      conf.write_to_file("RenegadeX.ini").unwrap();
-      conf
-    }
-  };
-  let section = conf.section(Some("RenX_Patcher".to_owned())).unwrap();
-  let game_location = section.get("GameLocation").unwrap();
-  let version_url = section.get("VersionUrl").unwrap();
-  let mut patcher : Downloader = Downloader::new();
-  patcher.set_location(game_location.to_string());
-  patcher.set_version_url(version_url.to_string());
-  patcher.retrieve_mirrors().unwrap();
-  patcher.rank_mirrors().expect(concat!(file!(),":",line!()));
-  match patcher.update_available().unwrap() {
-    Update::UpToDate => {
-      println!("Game up to date!");
-      //patcher.poll_progress();
-      //patcher.download().unwrap();
-    },
-    Update::Resume | Update::Delta | Update::Full | Update::Unknown => {
-      println!("Update available!");
-      patcher.poll_progress();
-      patcher.download().unwrap();
-    },
-    _ => {
+fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
-    }
-  }
-  Ok(())
+  let runtime : tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread().enable_all().thread_stack_size(2_usize.pow(22)).build().expect("");
+  let console_layer = console_subscriber::ConsoleLayer::builder().with_default_env().spawn();
+
+  tracing_subscriber::registry()
+    .with(console_layer)
+    .with(tracing_subscriber::fmt::layer().with_filter(tracing_subscriber::filter::LevelFilter::from_level(tracing::Level::DEBUG)))
+    .init();
+
+  runtime.block_on(async move {
+    let conf = match Ini::load_from_file("RenegadeX.ini") {
+      Ok(conf) => conf,
+      Err(_e) => {
+        let mut conf = Ini::new();
+        conf.with_section(Some("RenX_Patcher"))
+          .set("GameLocation", "D:/Renegade X/")
+          .set("VersionUrl", "https://static.ren-x.com/launcher_data/version/release.json");
+        conf.write_to_file("RenegadeX.ini").unwrap();
+        conf
+      }
+    };
+    let section = conf.section(Some("RenX_Patcher".to_owned())).unwrap();
+    let game_location = section.get("GameLocation").unwrap();
+    let version_url = section.get("VersionUrl").unwrap();
+  
+    let mut patcher_builder = renegadex_patcher::PatcherBuilder::new();
+    patcher_builder.set_software_location(game_location.to_string());
+    let version_information = crate::version_information::VersionInformation::retrieve(version_url).await?;
+    patcher_builder.set_software_information(version_information.software.mirrors, version_information.software.version, version_information.software.instructions_hash);
+  
+    patcher_builder.set_success_callback(Box::new(move || {
+      info!("Calling download done");
+    }));
+    patcher_builder.set_failure_callback(Box::new(move |error| {
+      error!("failure_callback {:#?}", &error);
+    }));
+    patcher_builder.set_progress_callback(Box::new(move |progress| {
+      let json = format!(
+        "{{\"action\": \"{}\",\"hash\": {{\"value\":{}, \"maximum\":{}}},\"download\": {{\"bytes\": {{\"value\":{}.0, \"maximum\":{}.0}}, \"files\": {{\"value\":{}, \"maximum\":{}}} }},\"patch\": {{\"value\":{}, \"ready\": {}, \"maximum\":{}}}}}",
+        progress.get_current_action().unwrap(),
+        progress.processed_instructions.0.load(Ordering::Relaxed),
+        progress.processed_instructions.1.load(Ordering::Relaxed),
+        0,
+        progress.downloaded_bytes.1.load(Ordering::Relaxed),
+        progress.downloaded_files.0.load(Ordering::Relaxed),
+        progress.downloaded_files.1.load(Ordering::Relaxed),
+        progress.patched_files.0.load(Ordering::Relaxed),
+        progress.patched_files.1.load(Ordering::Relaxed),
+        progress.patched_files.2.load(Ordering::Relaxed)
+      );
+      info!("{}", json);
+    }));
+  
+    let mut patcher = patcher_builder.build()?;
+  
+    patcher.start_patching().await;
+
+    tokio::signal::ctrl_c().await?;
+    Ok::<(), Box<dyn Error + Send + Sync + 'static>>(())
+  })
 }
